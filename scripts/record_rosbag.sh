709c913cdda4b3503c493619a8b2de1776a07fa4
BjarkeHJ <bjarke_holm@hotmail.com>
2026-03-16 11:03:31 +0100

    removed record script

--- scripts/record_rosbag.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PREFIX="ir_image_bag"
-TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
-OUTPUT_FOLDER="${PREFIX}_${TIMESTAMP}"
-TOPICS="/image_raw"
-
-cd ~/WN2640_IR_CAM/scripts
-ros2 bag record $TOPICS -o "$OUTPUT_FOLDER"
