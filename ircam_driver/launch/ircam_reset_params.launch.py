2b08aba47377d5f554a17ca41446bc891a3a07bc
BjarkeHJ <bjarke_holm@hotmail.com>
2026-04-08 10:11:30 +0200

    changes to new ircam

--- ircam_driver/launch/ircam_reset_params.launch.py
+++ ircam_driver/launch/ircam_reset_params.launch.py
@@ -6,15 +6,15 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
 
     config = os.path.join(
-        get_package_share_directory('ir_cam_bringup'),
+        get_package_share_directory('ircam_driver'),
         'config',
-        'reset_to_default_params.yaml'
+        'reset_to_default_params.yaml',
     )
 
     node = Node(
-        package='ir_cam_bringup',
-        executable='ir_cmd_uart',
-        name='ir_cmd_uart',
+        package='ircam_driver',
+        executable='ircam_cmd_uart',
+        name='ircam_cmd_uart',
         output='screen',
         parameters=[config]
     )
