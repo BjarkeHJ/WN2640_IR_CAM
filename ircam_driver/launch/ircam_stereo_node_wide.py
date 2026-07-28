52f20fed47ca3e375ac8611d3066ee449bbd0b8f
Jonathan Nielsen <jonathan@majj.dk>
2026-07-28 15:00:28 +0200

    updated workstation shutter for stereo ir cams

---
+++ ircam_driver/launch/ircam_stereo_node_wide.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    config = os.path.join(
+        get_package_share_directory('ircam_driver'),
+        'config',
+        'stereo_config_ir_1.yaml',
+    )
+
+    node = Node(
+        package='ircam_driver',
+        executable='ircam_cmd_uart',
+        name='ircam_cmd_uart',
+        output='screen',
+        parameters=[config]
+    )
+    
+    ld = LaunchDescription([node])
+
+    return ld
